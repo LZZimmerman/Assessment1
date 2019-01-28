5d8c017ddf666b34922e979ab447f01630b3cacf
LZZimmerman <43885846+LZZimmerman@users.noreply.github.com>
2019-01-28 10:41:07 +0000

    town interactions

--- test pythonscripts.py
+++ test pythonscripts.py
@@ -13,20 +13,30 @@ import random
 #     print("before loop break")
 #     inputcheck = False
 #     print("after loop break")
-testDict = {"Sword": 1, "Staff": 1}
-testList = ("Wooden Sword", "Wooden Staff", "Wooden Dagger")
-liststring = "\n".join(testList)
-print(liststring)
-print(testDict["Sword"])
+# testDict = {"Sword": 1, "Staff": 2}
+# testList = ("Wooden Sword", "Wooden Staff", "Wooden Dagger")
+# liststring = "\n".join(testList)
+# print(liststring)
+# print(testDict["Sword"])
+#
+# for i in range(0,3):
+#     print(i+1,"-",testList[i])
+# testChoice = 1
+# print("You have chosen to use your", testList[0])
+# print("You have chosen to use your", testList[testChoice])
+#
+# attackdmg = 0
+# intel = 20
+# staffMultiplier = int(testDict["Staff"])
+#
+# print("hey", attackdmg, "hey")
+# attackdmg = int(5+(intel*staffMultiplier)/10)
+# print(attackdmg)
+# print(staffMultiplier)
 
-for i in range(0,3):
-    print(i+1,"-",testList[i])
-testChoice = 1
-print("You have chosen to use your", testList[0])
-print("You have chosen to use your", testList[testChoice])
-
-attackdmg = 0
-intel = 19
-print("hey", attackdmg, "hey")
-attackdmg = int(5+(intel)/10)
-print(attackdmg)
+shopDict = {"Platinum staff": 200, "Gold staff": 100, "Bronze staff":50, "aluminium staff": 250}
+for k in sorted(shopDict.keys()):
+    print(k,shopDict[k])
+del(shopDict["Bronze staff"])
+for k in sorted(shopDict.keys()):
+    print(k,shopDict[k])
